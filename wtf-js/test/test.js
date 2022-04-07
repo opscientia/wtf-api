43041b540e3f3ef96c0c2ed7710491d1aac5cf93
Caleb <1calebtuttle@gmail.com>
2022-04-07 11:13:27 -0400

    Add to test suite

--- wtf-js/test/test.js
+++ wtf-js/test/test.js
@@ -1,34 +1,108 @@
 const { expect } = require('chai');
 
 const wtf = require('../lib/index');
+const { 
+  UnsupportedNetworkError,
+  UnsupportedServiceError,
+  CredentialsNotFoundError,
+  AddressNotFoundError
+} = require('../lib/errors');
 
+
+/**
+ * NOTE: These tests currently rely on startIntegration.js from DIDJWT.
+ *       The script must be running in order for these tests to pass.
+ */
 
 describe('wtf-js', function () {
 
   // TODO: before(run startIntegration.js) // update startIntegration to take creds and addresses
   //       Make startIntegration.js a self-contained script/module
+  before(function () {
+    this.userAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+    this.orcid = '0000-0002-2308-9517'
+    this.gmail = 'nanaknihal@gmail.com'
+  });
 
   describe('credentialsForAddress', function () {
     it('Should return correct credentials (orcid)', async function () {
-      const creds = await wtf.credentialsForAddress('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', 'ethereum', 'orcid');
-      expect(creds).to.equal('0000-0002-2308-9517');
+      const creds = await wtf.credentialsForAddress(this.userAddress, 'ethereum', 'orcid');
+      expect(creds).to.equal(this.orcid);
     });
     
     it('Should return correct credentials (google)', async function () {
-      const creds = await wtf.credentialsForAddress('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', 'ethereum', 'google');
-      expect(creds).to.equal('nanaknihal@gmail.com');
+      const creds = await wtf.credentialsForAddress(this.userAddress, 'ethereum', 'google');
+      expect(creds).to.equal(this.gmail);
+    });
+
+    it("Should throw UnsupportedNetworkError when network=='bitcoin'", async function () {
+      // TODO: Replace with chai's expect().to.throw()
+      try {
+        await wtf.credentialsForAddress(this.userAddress, 'bitcoin', 'google')
+      }
+      catch (err) {
+        expect(err.message).to.equal(UnsupportedNetworkError('bitcoin').message)
+      }
+    });
+
+    it("Should throw UnsupportedServiceError when service=='randoService'", async function () {
+      try {
+        await wtf.credentialsForAddress(this.userAddress, 'ethereum', 'randoService')
+      }
+      catch (err) {
+        expect(err.message).to.equal(UnsupportedServiceError('ethereum', 'randoService').message)
+      }
+    });
+
+    it("Should throw CredentialsNotFoundError when querying for unfound credentials", async function () {
+      const badAddress = '0x1234567891234567891234567891234567891234'
+      try {
+        await wtf.credentialsForAddress(badAddress, 'ethereum', 'orcid')
+      }
+      catch (err) {
+        expect(err.message).to.equal(CredentialsNotFoundError('ethereum', 'orcid', badAddress).message)
+      }
     });
   });
 
   describe('addressForCredentials', function () {
     it('Should return correct address (orcid)', async function () {
-      const address = await wtf.addressForCredentials('ethereum', 'nanaknihal@gmail.com', 'google');
-      expect(address.toString().toLowerCase()).to.equal('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266');
+      const address = await wtf.addressForCredentials('ethereum', this.gmail, 'google');
+      expect(address).to.equal(this.userAddress);
     });
 
     it('Should return correct address (google)', async function () {
       const address = await wtf.addressForCredentials('ethereum', '0000-0002-2308-9517', 'orcid');
-      expect(address.toString().toLowerCase()).to.equal('0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266');
+      expect(address).to.equal(this.userAddress);
+    });
+
+    it("Should throw UnsupportedNetworkError when network=='bitcoin'", async function () {
+      // TODO: Replace with chai's expect().to.throw()
+      try {
+        await wtf.addressForCredentials('bitcoin', this.gmail, 'google')
+      }
+      catch (err) {
+        expect(err.message).to.equal(UnsupportedNetworkError('bitcoin').message)
+      }
+    });
+
+    it("Should throw UnsupportedServiceError when service=='randoService'", async function () {
+      try {
+        await wtf.addressForCredentials('ethereum', this.gmail, 'randoService')
+      }
+      catch (err) {
+        expect(err.message).to.equal(UnsupportedServiceError('ethereum', 'randoService').message)
+      }
+    });
+
+    it("Should throw CredentialsNotFoundError when querying for unfound credentials", async function () {
+      const badCredentials = '123abc'
+      try {
+        await wtf.addressForCredentials('ethereum', badCredentials, 'orcid')
+      }
+      catch (err) {
+        expect(err.message).to.equal(AddressNotFoundError('ethereum', 'orcid', badCredentials).message)
+      }
     });
   });
   
@@ -37,9 +111,8 @@ describe('wtf-js', function () {
       const addresses = await wtf.getAllUserAddresses();
       expect(addresses).to.be.an('object').that.has.all.keys('ethereum');
       expect(addresses['ethereum']).to.be.an('object').that.has.keys('orcid', 'google');
-      expect(addresses['ethereum']['orcid']).to.be.an('array').that.has.members(['0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266']);
-      expect(addresses['ethereum']['google']).to.be.an('array').that.has.members(['0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266']);
+      expect(addresses['ethereum']['orcid']).to.be.an('array').that.has.members([this.userAddress]);
+      expect(addresses['ethereum']['google']).to.be.an('array').that.has.members([this.userAddress]);
     });
   });
 });
-
