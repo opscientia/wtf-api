781034ece2fd810acef67d92107e06619cc7d421
calebtuttle <1calebtuttle@gmail.com>
2022-04-23 13:20:13 -0400

    Continue refactor

--- wtf-js/lib/index.js
+++ wtf-js/lib/index.js
@@ -307,9 +307,7 @@ function wtf() {
         crossChainHolo[network] = credsNameBio;
       }
       catch (err) {
-        console.log(err);
-        console.log(`An error occurred when calling IdentityAggregator on network "${network}". It is possible that ` +
-                    "the provider you are using does not support one of the networks used by WTF.");
+        logFailedContractCall(err, 'IdentityAggregator', network)
         crossChainHolo[network] = {};
       }
     }
