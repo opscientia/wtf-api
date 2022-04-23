92a24e68eaee7464ee68b027cded0a160aa92f31
calebtuttle <1calebtuttle@gmail.com>
2022-04-23 11:48:00 -0400

    Add utils

---
+++ wtf-js/lib/utils/utils.js
@@ -0,0 +1,7 @@
+
+module.exports.logContractCallFailed = (error, contract, network) => {
+  console.log(error);
+  console.log(`An error occurred when calling ${contract} on network "${network}". `
+              + `It is possible that the provider you are using does not support `
+              + `one of the networks used by WTF.`);
+}
